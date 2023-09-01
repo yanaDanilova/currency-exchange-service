555947f117f399d5c3ae4e8dc721aa2c397af09f
yanad <yana.danilova1509@gmail.com>
2023-09-01 13:34:07 +0200

    last step

--- src/main/java/com/in28minuten/microsevices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/in28minuten/microsevices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.in28minuten.microsevices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger loger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     @Autowired
     private CurrencyExchangeRepository repository;
     @Autowired
@@ -18,6 +22,9 @@ public class CurrencyExchangeController {
     public CurrencyExchange getExchangeValue(@PathVariable String from, @PathVariable String to){
         /*CurrencyExchange currencyExchange = new CurrencyExchange(1000l, from, to, BigDecimal.valueOf(50));
         return currencyExchange;*/
+
+        //2023-09-01T12:41:20.676+02:00  INFO [currency-exchange,4c4643f06be602455e10dceab1f6cf1a,58ddbd0cb6186fd5] 2532 --- [nio-8000-exec-1] c.i.m.c.CurrencyExchangeController       : getExchange value called with USD to INR
+        loger.info("getExchange value called with {} to {}",from,to);
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from,to);
         if(currencyExchange==null){
             throw new RuntimeException("Unable to find data for "+ from+ " to "+to);
